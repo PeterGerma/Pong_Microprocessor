7c6a1a0231de380574754fb19b750d500684ec0d
Pierre Germa <pgerma@hypnos.andrew.local.cmu.edu>
2017-11-28 16:20:52 -0500

    Initial Synthesizable Verilog

--- sadd2017.asm
+++ sadd2017.asm
@@ -1,7 +1,7 @@
 ; Saturating addition benchmark
 ; Works by cumulatively adding values in the "values array"
 ; After each saturating addition, the result is placed in the "results array"
-; Saturating addition done by explicit test: checking V flag, then N flag and 
+; Saturating addition done by explicit test: checking V flag, then N flag and
 ;    changing any result to $7FFF or $8000
          .ORG   $1000
 
@@ -9,22 +9,22 @@
          LDI   R3,$0000  ; initial sum value
          LDI   R1,DATA   ; address of values array
          LDI   R4, SUM   ; address of results array
-LOOP     LDR   R2,R1      
-         ADD   R3,R2      
+LOOP     LDR   R2,R1
+         ADD   R3,R2
          BRV   SAT       ; V because these values are signed
 CKEND    STR   R4, R3    ; Store sum in results array
          INCR  R1
-         INCR  R4     
-         DECR  R0     
+         INCR  R4
+         DECR  R0
          BRN   DONE      ; done with adding?
-         BRA   LOOP     
+         BRA   LOOP
 DONE     STOP            ; Yep, done
-SAT      BRN   NEGTVE    ; 
+SAT      BRN   NEGTVE    ;
          LDI   R3,$8000  ; Put value in range
-         BRA   CKEND     ; 
+         BRA   CKEND     ;
 NEGTVE   LDI   R3,$7FFF  ; Put value in range
-         BRA   CKEND     ; 
-    
+         BRA   CKEND     ;
+
          .ORG $2000
 
 DATA     .DW $DEAD
@@ -41,7 +41,7 @@ DATA     .DW $DEAD
          .DW $000F
          .DW $FFF2
          .DW $0010
-         
+
 SUM      .DW $FF    ; Expect $DEAD
          .DW $FF    ; Expect $9D9C
          .DW $FF    ; Expect $8000
@@ -58,7 +58,7 @@ SUM      .DW $FF    ; Expect $DEAD
          .DW $FF    ; Expect $7FFF
          .DW $FF    ; Expect no change
          .DW $00
-         
-         
-  
-    
+
+
+
+
